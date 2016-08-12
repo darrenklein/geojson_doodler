b85fd9d1f0eb51a4c66405f50ca5494b12dfd77d
Darren Klein <darren.a.klein@gmail.com>
2016-08-12 00:23:05 -0400

    Commit success! Started on a better method... needs work, though

--- scripts/geojson_doodler.js
+++ scripts/geojson_doodler.js
@@ -206,47 +206,57 @@ function exportTableToCSV($table, filename){
 };
 
 
-function exportTableToGeoJSON($table, filename){
+//function exportTableToGeoJSON($table, filename){
+//
+//    var $rows = $table.find('tr:has(td)'),
+//
+//        // Temporary delimiter characters unlikely to be typed by keyboard
+//        // This is to avoid accidentally splitting the actual contents
+//        tmpColDelim = String.fromCharCode(11), // vertical tab character
+//        tmpRowDelim = String.fromCharCode(0), // null character
+//
+//        // actual delimiter characters for CSV format
+//        colDelim = '","',
+//        rowDelim = '"\r\n"',
+//
+//        // Grab text from table into CSV formatted string
+//        csv = $rows.map(function (i, row) {
+//            var $row = $(row),
+//                $cols = $row.find('td');
+//
+//            return $cols.map(function (j, col) {
+//                var $col = $(col),
+//                    text = $col.text();
+//
+//                return text.replace(/"/g, '""'); // escape double quotes
+//
+//            }).get().join(tmpColDelim);
+//
+//        }).get().join(tmpRowDelim)
+//            .split(tmpRowDelim).join(rowDelim)
+//            .split(tmpColDelim).join(colDelim),
+//
+//        // Data URI
+//        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+//
+//    $(this)
+//        .attr({
+//        'download': filename,
+//        'href': csvData,
+//        'target': '_blank'
+//    });
+//};
 
-    var $rows = $table.find('tr:has(td)'),
 
-        // Temporary delimiter characters unlikely to be typed by keyboard
-        // This is to avoid accidentally splitting the actual contents
-        tmpColDelim = String.fromCharCode(11), // vertical tab character
-        tmpRowDelim = String.fromCharCode(0), // null character
+function exportRowToGeoJSON(filename, elId, mimeType) {
+    var elHtml = $(elId).html();
+    var link = document.createElement('a');
+    mimeType = mimeType || 'text/plain';
 
-        // actual delimiter characters for CSV format
-        colDelim = '","',
-        rowDelim = '"\r\n"',
-
-        // Grab text from table into CSV formatted string
-        csv = $rows.map(function (i, row) {
-            var $row = $(row),
-                $cols = $row.find('td');
-
-            return $cols.map(function (j, col) {
-                var $col = $(col),
-                    text = $col.text();
-
-                return text.replace(/"/g, '""'); // escape double quotes
-
-            }).get().join(tmpColDelim);
-
-        }).get().join(tmpRowDelim)
-            .split(tmpRowDelim).join(rowDelim)
-            .split(tmpColDelim).join(colDelim),
-
-        // Data URI
-        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
-
-    $(this)
-        .attr({
-        'download': filename,
-        'href': csvData,
-        'target': '_blank'
-    });
-};
-
+    link.setAttribute('download', filename);
+    link.setAttribute('href', 'data:' + mimeType  +  ';charset=utf-8,' + encodeURIComponent(elHtml));
+    link.click(); 
+}
 
 function clearMap(){
     $.each(featureGroup._layers, function(){
