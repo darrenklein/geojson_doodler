9fd76961830969b67ef716fb6d5f918351c06c89
Darren Klein <darren.a.klein@gmail.com>
2016-08-11 23:46:29 -0400

    Commit success! Got down th every basics of exporting as GeoJSONs rather than only.csv

--- scripts/geojson_doodler.js
+++ scripts/geojson_doodler.js
@@ -206,6 +206,48 @@ function exportTableToCSV($table, filename){
 };
 
 
+function exportTableToGeoJSON($table, filename){
+
+    var $rows = $table.find('tr:has(td)'),
+
+        // Temporary delimiter characters unlikely to be typed by keyboard
+        // This is to avoid accidentally splitting the actual contents
+        tmpColDelim = String.fromCharCode(11), // vertical tab character
+        tmpRowDelim = String.fromCharCode(0), // null character
+
+        // actual delimiter characters for CSV format
+        colDelim = '","',
+        rowDelim = '"\r\n"',
+
+        // Grab text from table into CSV formatted string
+        csv = $rows.map(function (i, row) {
+            var $row = $(row),
+                $cols = $row.find('td');
+
+            return $cols.map(function (j, col) {
+                var $col = $(col),
+                    text = $col.text();
+
+                return text.replace(/"/g, '""'); // escape double quotes
+
+            }).get().join(tmpColDelim);
+
+        }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim),
+
+        // Data URI
+        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+    $(this)
+        .attr({
+        'download': filename,
+        'href': csvData,
+        'target': '_blank'
+    });
+};
+
+
 function clearMap(){
     $.each(featureGroup._layers, function(){
         featureGroup.removeLayer(this)
